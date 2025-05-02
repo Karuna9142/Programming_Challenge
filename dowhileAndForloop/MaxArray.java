e3ff9330fb7cd49cf1dbaacc4a174a6b794f1c52
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-05-02 22:32:03 +0530

    Add files via upload

---
+++ dowhileAndForloop/MaxArray.java
@@ -0,0 +1,22 @@
+package com.dowhileAndForloop;
+
+import com.array.ArrayUtility;
+
+public class MaxArray {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to finding the maximum");
+		int [] arr = ArrayUtility.inputArray();
+		
+		int max = Integer.MIN_VALUE;
+		for(int num : arr) {
+			if(max < num) {
+				max = num;
+			}
+		}
+		
+       System.out.println("Maximum number is:- "+max);
+	}
+
+}
