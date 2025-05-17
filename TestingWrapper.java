b52c1ceff89b19219156db62e81d79947945cc0a
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-05-17 22:29:08 +0530

    Add files via upload

---
+++ TestingWrapper.java
@@ -0,0 +1,12 @@
+package com.collections;
+
+public class TestingWrapper {
+
+	public static void main(String[] args) {
+		
+		//we can convert it any data type
+		Integer first = Integer.valueOf(88);
+
+	}
+
+}
