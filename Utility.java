c5dfd362f60376c48e2ed7739066835fea541456
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-05-17 22:28:30 +0530

    Add files via upload

---
+++ Utility.java
@@ -0,0 +1,16 @@
+package com.collections;
+
+import java.util.Collection;
+
+public class Utility {
+
+	public static <E>void print(Collection<E> collection) {
+	  
+		System.out.print("Collection is: ");
+		for(E coll : collection) {
+			System.out.printf("%s ", coll.toString());
+		}
+
+	}
+
+}
